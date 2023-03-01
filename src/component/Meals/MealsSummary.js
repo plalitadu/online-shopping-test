d63dc47ddb80a694906f5556720235bdede270c6
Lalita Dulma <lalita.du@sansiri.com>
2023-03-01 21:59:30 +0700

    update

--- src/component/Meals/MealsSummary.js
+++ src/component/Meals/MealsSummary.js
@@ -5,7 +5,7 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>FULLTIME ONLINE SHOPPING</h2>
       <p>
-        Your Bestfriend Online Shopping !!
+        3.3 Campaign!! SUPER DEALS !!!
       </p>
       {/* <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
